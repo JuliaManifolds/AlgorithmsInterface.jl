e456f0d68a5d18178b8286d9804b48ff646d1b61
Ronny Bergmann <git@ronnybergmann.net>
2026-04-30 15:09:37 +0200

    codecov++

--- test/stopping_criterion.jl
+++ test/stopping_criterion.jl
@@ -24,18 +24,23 @@ problem = AIT.DummyProblem()
 end
 
 @testset "StopAfter" begin
-    s1 = StopAfter(Second(1))
+    s1 = StopAfter(Nanosecond(7))
     @test s1 isa StoppingCriterion
-    @test string(s1) == "StopAfter(Second(1))"
+    @test string(s1) == "StopAfter(Nanosecond(7))"
     @test_throws ArgumentError StopAfter(Second(-1))
 
     algorithm = AIT.DummyAlgorithm(s1)
     s1_state = initialize_state(problem, algorithm, s1)
-    alg_state = AIT.DummyState(s1_state, 1)
+    alg_state = AIT.DummyState(s1_state, 0)
+    # Iteration 0: Start timer
+    @test !is_finished!(problem, algorithm, alg_state)
     @test !is_finished(problem, algorithm, alg_state)
-    s1_state.time = Second(2)
+    @test isnothing(get_reason(s1, s1_state))
+    # Fake stop
+    s1_state.time = Nanosecond(9)
+    alg_state.iteration = 2
+    @test is_finished!(problem, algorithm, alg_state)
     @test is_finished(problem, algorithm, alg_state)
-    s1_state.at_iteration = 2
     @test startswith(get_reason(s1, s1_state), "After iteration 2")
     @test endswith(summary(s1, s1_state), "    reached")
 end
@@ -64,6 +69,7 @@ end
     s1_state.criteria_states[2].start = Nanosecond(time_ns())
     s1_state.criteria_states[2].time = Nanosecond(7)
     # just time is not enough
+    @test !is_finished!(problem, algorithm, alg_state)
     @test !is_finished(problem, algorithm, alg_state)
     alg_state.iteration = 2
     # but now both are
@@ -71,6 +77,8 @@ end
     @test !indicates_convergence(s1)
     # check that reset works (a) check with modification
     @test is_finished!(problem, algorithm, alg_state)
+    @test is_finished(problem, algorithm, alg_state)
+    @test startswith(get_reason(s1, s1_state), "At iteration 2")
     @test alg_state.stopping_criterion_state.at_iteration > 0
     AlgorithmsInterface.initialize_state!(problem, algorithm, s1, s1_state)
     @test s1_state.criteria_states[1].at_iteration == -1
@@ -102,6 +110,7 @@ end
 
     @test isnothing(AlgorithmsInterface.get_reason(s1, s1_state))
     alg_state = AIT.DummyState(s1_state, 1)
+    @test !is_finished!(problem, algorithm, alg_state)
     @test !is_finished(problem, algorithm, alg_state)
     s1_state.criteria_states[2].time = Second(2)
     @test is_finished(problem, algorithm, alg_state)
